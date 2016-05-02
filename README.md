0c4dc5e2300d233e91740c5ab3cca12b587f4095
operando <okanoshinobu8@gmail.com>
2016-05-02 20:35:02 +0900

    commit

--- README.md
+++ README.md
@@ -10,7 +10,3 @@ DeployGate + Travis CIの構成
 
 * http://qiita.com/hogelog/items/e24591cf9c2abac4712b
 * http://henteko07.hatenablog.com/entry/2014/03/12/231851
-
-## テスト
-
-hogehoge
---
+++ app/src/main/java/com/operando/os/MainActivity.java
@@ -0,0 +1,15 @@
+package com.operando.os;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import atte.android.souzoh.com.deploygatesample.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
